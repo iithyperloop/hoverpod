819b7f9c0b1a70f892b149871290012d0a4bb036
xgladius <stormwizard1001@gmail.com>
2022-02-09 22:40:06 -0600

    threading is hard :(

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,28 @@
-// this is mostly just for cmake.
 #include "HeightSensor/HeightSensor.hpp"
 #include "IMU/IMU.hpp"
 #include "Motor/Motor.hpp"
 #include "Actuator/Actuator.hpp"
+#include "Threading/Threading.hpp"
+#include <chrono>
+
+void kalman_filter(std::atomic<double>& val) {
+	while (true) {
+		std::cout << "Side thread: We executed!\n" << std::endl;
+		const auto orig_val = val.load();
+		val.store(orig_val + 1);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
+}
 
 int main() {
-    std::cout << "for cmake" << std::endl;
+	Hyperloop::Thread<double> KalmanFilter(kalman_filter, 0);
+	KalmanFilter.run_asynchronously();
+	while (true) {
+		double val = KalmanFilter.get();
+		printf("Main thread: %f\n", val);
+		if (val == 100000)
+			break;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
     return 1;
 }
---
+++ Threading/Threading.hpp
@@ -0,0 +1,30 @@
+#include <thread>
+#include <atomic>
+#include <functional>
+
+namespace Hyperloop {
+
+	template <class T>
+	class Thread {
+		std::atomic<T> val;
+		std::thread thread;
+	public:
+		Thread(void (*F)(std::atomic<T> &val), T initial_val) {
+			val.store(initial_val);
+			thread = std::thread(F, std::ref(val));
+		}
+
+		T get() {
+			return val.load();
+		}
+
+		void run_asynchronously() {
+			thread.detach();
+		}
+
+		void run_synchronously() {
+			thread.attach();
+		}
+	};
+
+}
