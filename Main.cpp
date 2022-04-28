762e7e4facf8835c56eb51e12d2f2ff69acf6841
Braeden <bweaver4@hawk.iit.edu>
2022-04-27 23:54:59 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,10 +8,34 @@ int main() {
         init_gui(); // do gui (which has loop inside of it)
     });
     t.detach(); // run seperate from main thread
-
-    IMU imu; // instantiate imu object
+    
     while (true) { // run forever because if main thread exists, side thread exists
-        imu.do_imu(); // do imu
+        #ifdef JOYSTICK
+        cout << "in Joystick controller" << endl;
+        // Input from button goes 1-0 then we turn on
+        if (buttonState == 1) {
+          if (buttonState == 0 ) {
+              for (int i = 0; i < takeOffSpeed; i++) {
+                  increaseSpeed(i);
+              }
+              if (buttonState == 1) {
+                  if (buttonState == 0) {
+                      for (int j = 0; j > 0; j++) {
+                          decreaseSpeed(j);
+                      }
+                  }
+              }
+          }
+        }
+        #endif
+        #ifdef GUI
+        cout << "in GUI controller" << endl;  #endif
+        #endif
+
+        #ifdef IMU
+        cout << "in Joystick controller" << endl;
+        #endif
     }
+
     return 1; // success (should never reach this)
 }
