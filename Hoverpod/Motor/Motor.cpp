f93c3327e703a075b78a7f1b8ee2a941ba298986
Faith Busche <56886581+fbusche@users.noreply.github.com>
2022-04-28 05:26:03 -0500

    Update Motor.cpp
    took the nested if that was used for what to do when button pressed and turned into a function. now this can be repeated called in i2c.

--- Hoverpod/Motor/Motor.cpp
+++ Hoverpod/Motor/Motor.cpp
@@ -1,19 +1,22 @@
 #include "Motor.hpp"
+#include <iostream>
 
 // by motor we mean engines
 // this will take button input and turn engine on/off based on that
 
-int buttonState = 1;
-float takeOffSpeed = 10; // will change later
 
-Motor::Motor() : speed(0) {}
+Motor::Motor() {
+    int buttonState = 1;
+    const float takeOffSpeed = 10; // will change value once tested
+    float speed;
+}
 
-double Motor::getSpeed() const {
+double Motor::getSpeed() {
     return speed;
 }
 
-void Motor::setSpeed(const double val) {
-    speed = val > 0 ? val : 0;
+void Motor::setSpeed(double val) {
+    speed = val > 0 ? val : 0; //making sure that speed is pos val
 }
 
 void Motor::increaseSpeed(double val) {
@@ -24,3 +27,32 @@ void Motor::decreaseSpeed(double val) {
     setSpeed(getSpeed() - val);
 }
 
+int Motor::getButtonState() {
+    return buttonState;
+}
+
+void Motor::setButtonState(int val) {
+    buttonState = val;
+}
+
+void Motor::turnMotorOn() {
+    
+// Input from button goes 1-0 then we turn on
+    if (getButtonState() == 1) {
+        if (getButtonState() == 0) {
+            if (getSpeed() == 0) {
+                for (int i = 0; i < takeOffSpeed(); i++) {
+                    increaseSpeed(i);
+                }
+            }
+            else {
+                int currentSpeed = getSpeed();
+                for (int j = 0; j < currentSpeed; j++) {
+                    decreaseSpeed(j);
+                }
+            }
+            
+        }
+    }
+
+}
