3775925319b5428a60366f723b1bc2400ff9d5f7
Tejash Panda <52340554+Tpanda03@users.noreply.github.com>
2022-04-28 03:33:37 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,8 @@ int main() {
         while (true) { // run forever because if main thread exists, side thread exists
             this_thread::sleep_for(chrono::milliseconds(3));
             #ifdef JOYSTICK
+            #include "bus.h"
+            #include "i2c.h"
             
             /*
             my thought process:
@@ -17,6 +19,13 @@ int main() {
               joystick data comes in as x,y,z and 1/0 where x-->x_axis, y-->y_axis, z-->roatation || button = HIGH/LOW,
               arduino parses data and sends to i2c using a Master-Slave connection (ask Electrical team for details)
 
+              float[]
+
+              return [a, b];
+
+              float[0] = a;
+              float[1] = b;
+
 
               **FOR XYZ**
               jetson recieves data in this class
@@ -24,38 +33,30 @@ int main() {
                   this is communicating with the actuator class where the commands ^ exists
 
                **FOR Button**
-               jetson recieves a HIGH when button is pressed 
+               jetson recieves a HIGH when button is pressed
                if motors are off:
                     turnMotorOn() (ask Electrical team for details)
                else:
                     turnMotorOff() (ask Electrical team for details)
 
+                potentiometer is also looking at position?
+
+
             */
-            
-            cout << "in Joystick controller" << endl;
-            // Input from button goes 1-0 then we turn on
-            if (buttonState == 1) {
-            if (buttonState == 0 ) {
-                for (int i = 0; i < takeOffSpeed; i++) {
-                    increaseSpeed(i);
-                }
-                if (buttonState == 1) {
-                    if (buttonState == 0) {
-                        for (int j = 0; j > 0; j++) {
-                            decreaseSpeed(j);
-                        }
-                    }
-                }
+
+              cout << "in Joystick controller" << endl;
+
+            #endif
             }
          }
-            
+
     });
     t.detach(); // run seperate from main thread
-    
+
     init_gui(); // do gui (which has loop inside of it)
 
     /*IMU imu;
-    std::thread t1([&imu] { // [] {} == lambda 
+    std::thread t1([&imu] { // [] {} == lambda
         imu.do_imu(); // do gui (which has loop inside of it)
     });
     t1.detach(); // run seperate from main thread*/
