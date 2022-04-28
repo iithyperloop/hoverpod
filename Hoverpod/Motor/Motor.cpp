3093d1e2bc11ae276e1b4ac08f968b033cf126ae
Faith Busche <56886581+fbusche@users.noreply.github.com>
2022-04-27 19:26:54 -0500

    Update Motor.cpp
    added nested if statement for take off and landing. need to most likely change takeOff speed variable based on testing because we don't know how much we have to run motors to make pod lift off. also will have to make sure this if statement is continuously being checked based on the data from joystick.

--- Hoverpod/Motor/Motor.cpp
+++ Hoverpod/Motor/Motor.cpp
@@ -1,5 +1,11 @@
 #include "Motor.hpp"
 
+// by motor we mean engines
+// this will take button input and turn engine on/off based on that
+
+int buttonState = 1;
+float takeOffSpeed = 10; // will change later
+
 Motor::Motor() : speed(0) {}
 
 double Motor::getSpeed() const {
@@ -16,4 +22,20 @@ void Motor::increaseSpeed(double val) {
 
 void Motor::decreaseSpeed(double val) {
     setSpeed(getSpeed() - val);
+}
+
+// Input from button goes 1-0 then we turn on
+if (buttonState == 1) {
+    if (buttonState == 0 ) {
+        for (int i = 0; i < takeOffSpeed; i++) {
+            increaseSpeed(i);
+        }
+        if (buttonState == 1) {
+            if (buttonState == 0) {
+                for (int j = 0; j > 0; j++) {
+                    decreaseSpeed(j);
+                }
+            }
+        }
+    }
 }
