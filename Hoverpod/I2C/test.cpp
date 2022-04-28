35bf34b2ebc2ff27748e22c06accf1b51fb05fb6
Tejash Panda <52340554+Tpanda03@users.noreply.github.com>
2022-04-28 09:14:52 -0500

    Update test.cpp

--- Hoverpod/I2C/test.cpp
+++ Hoverpod/I2C/test.cpp
@@ -1,62 +1,63 @@
-/*============================================================================
- *  Name        : BlinkSample.cpp
- *  Author      : praful vanker
- *  Version     :
- *  Copyright   : GPL-3.0-only
- *  			  Everyone is permitted to copy and distribute verbatim copies
- *  			  of this license document, but changing it is not allowed.
- *  Description : Blink LED connected to Pin 40
-============================================================================*/
+#include "Hoverpod/GUI/GUI.hpp"
+#include "Hoverpod/IMU/IMU.hpp"
 
-#ifdef __linux__
-#include <iostream>
-#include <unistd.h>
+int main() {
+    // https://en.cppreference.com/w/cpp/thread/thread
+    // [] is lambda declaration (inline anonymous function)
+
+    std::thread t([] { // [] {} == lambda
+        while (true) { // run forever because if main thread exists, side thread exists
+            this_thread::sleep_for(chrono::milliseconds(3));
+            #ifdef JOYSTICK
+            #include "bus.h"
+            #include "i2c.h"
+
+            /*
+            my thought process:
+              communication: Joystick --> arduino --> jetson --> actuators/motors
+
+              joystick data comes in as x,y,z and 1/0 where x-->x_axis, y-->y_axis, z-->roatation || button = HIGH/LOW,
+              arduino parses data and sends to i2c using a Master-Slave connection (ask Electrical team for details)
+
+              float[]
+
+              return [a, b];
+
+              float[0] = a;
+              float[1] = b;
 
 
-#ifdef JOYSTICK
-#include "thrustVectoring.h"
-#endif
+              **FOR XYZ**
+              jetson recieves data in this class
+                  jetson determines what the pod should do i.e. moveForward(), moveBackward(), moveLeft(), moveRight(), rotateCW(), rotateCCW()
+                  this is communicating with the actuator class where the commands ^ exists
+
+               **FOR Button**
+               jetson recieves a HIGH when button is pressed
+               if motors are off:
+                    turnMotorOn() (ask Electrical team for details)
+               else:
+                    turnMotorOff() (ask Electrical team for details)
+
+                potentiometer is also looking at position?
 
 
-#ifdef GUI
-#include "GUI.h"
-#endif
+            */
 
-#ifdef IMU
-#include "IMU.h"
-#endif
+              cout << "in Joystick controller" << endl;
 
-//using namespace pmvankerNano;
+            #endif
+            }
+    });
+    t.detach(); // run seperate from main thread
 
-/*int main() {
+    init_gui(); // do gui (which has loop inside of it)
 
-  #ifdef JOYSTICK
-  cout << "in Joystick controller" << endl;
-  // Input from button goes 1-0 then we turn on
-  if (buttonState == 1) {
-      if (buttonState == 0 ) {
-          for (int i = 0; i < takeOffSpeed; i++) {
-              increaseSpeed(i);
-          }
-          if (buttonState == 1) {
-              if (buttonState == 0) {
-                  for (int j = 0; j > 0; j++) {
-                      decreaseSpeed(j);
-                  }
-              }
-          }
-      }
-  }
-  #endif
+    /*IMU imu;
+    std::thread t1([&imu] { // [] {} == lambda
+        imu.do_imu(); // do gui (which has loop inside of it)
+    });
+    t1.detach(); // run seperate from main thread*/
 
-  #ifdef GUI
-  cout << "in GUI controller" << endl;  #endif
-  #endif
-    
-  #ifdef IMU
-  cout << "in Joystick controller" << endl;
-  #endif
-  
-  return 0;
-}*/
-#endif
+    return 1; // success (should never reach this)
+}
