a3abf0ace83570fc4d8cfa9b02528fe218ea988a
Braeden <bweaver4@hawk.iit.edu>
2022-04-27 19:18:47 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,14 +2,16 @@
 #include "Hoverpod/IMU/IMU.hpp"
 
 int main() {
-    std::thread t([ ] {
-        init_gui();
+    // https://en.cppreference.com/w/cpp/thread/thread
+    // [] is lambda declaration (inline anonymous function)
+    std::thread t([] { // [] {} == lambda 
+        init_gui(); // do gui (which has loop inside of it)
     });
-    t.detach();
+    t.detach(); // run seperate from main thread
 
-    IMU imu;
-    while (true) {
-        imu.do_imu();
+    IMU imu; // instantiate imu object
+    while (true) { // run forever because if main thread exists, side thread exists
+        imu.do_imu(); // do imu
     }
-    return 1;
+    return 1; // success (should never reach this)
 }
